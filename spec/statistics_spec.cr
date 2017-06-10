42c34e309954e5a6fb1a6cc4ed8b135d2a3c98b1
Hugo Abonizio <hugo_abonizio@hotmail.com>
2017-06-10 10:34:03 -0300

    feature: Add #variance and #standard_deviation

--- spec/statistics_spec.cr
+++ spec/statistics_spec.cr
@@ -17,4 +17,18 @@ describe Statistics do
     [1, 2, 3, 4, 5, 6].median.should eq(3.5)
     [1, 20, 50, 60, 10].median.should eq(20)
   end
+
+  it "#variance" do
+    [2, 2, 2].variance.should eq(0)
+    [1, 2, 3, 4].variance.should eq(1.25)
+    [5, 10, 15].variance.should be_close(16.666666666666668, 0.001)
+    [-1, 0, 1].variance.should be_close(0.6666666666666666, 0.001)
+  end
+
+  it "#standard_deviation" do
+    [2, 2, 2].standard_deviation.should eq(0)
+    [1, 2, 3, 4].standard_deviation.should be_close(1.1180339887499, 0.001)
+    [5, 10, 15].standard_deviation.should be_close(4.08248, 0.001)
+    [-1, 0, 1].standard_deviation.should be_close(0.81649658093, 0.001)
+  end
 end
--- src/statistics.cr
+++ src/statistics.cr
@@ -11,4 +11,13 @@ module Statistics
     return sorted[length / 2] if length.odd?
     (sorted[length / 2] + sorted[length / 2 - 1]) / 2.0
   end
+
+  def variance
+    m = mean
+    map { |i| (m - i.to_f) ** 2 }.sum.to_f / size
+  end
+
+  def standard_deviation
+    variance ** 0.5
+  end
 end
